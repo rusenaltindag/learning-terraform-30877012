584eb6c7be070bffdcb1f9300390fa5a2c6a8582
Rusen <r.altindag95@gmail.com>
2024-01-16 11:49:53 +0100

    fix comma

--- main.tf
+++ main.tf
@@ -24,7 +24,7 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a,", "us-west-2b,", "us-west-2c", "us-west-2d"]
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c", "us-west-2d"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
